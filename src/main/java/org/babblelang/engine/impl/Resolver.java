3cd0e36770e054a4a366f4ecdeb3aa666566e7d8
Nicolas Lehuen <nicolas@lehuen.com>
2013-06-25 23:01:47 +0200

    Remove boilerplate comment.

--- src/main/java/org/babblelang/engine/impl/Resolver.java
+++ src/main/java/org/babblelang/engine/impl/Resolver.java
@@ -1,12 +1,5 @@
 package org.babblelang.engine.impl;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Nico
- * Date: 24/06/13
- * Time: 19:41
- * To change this template use File | Settings | File Templates.
- */
 public interface Resolver {
     boolean isDeclared(String key);
 
